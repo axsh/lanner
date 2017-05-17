d61772db99d1624618d447f923dd4f9f940b6a30
rakshasa <sundell.software@gmail.com>
2017-05-17 16:59:48 +0900

    Preseed files for ubuntu.

--- ubuntu/convert_ubuntu.sh
+++ ubuntu/convert_ubuntu.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 SCRIPT_DIR=`dirname ${BASH_SOURCE[0]}`
-
 BASE_PATH="${1:?Root directory for Ubuntu USB not provided.}"
 
 BOOT_FILES="
@@ -13,26 +12,43 @@ BOOT_FILES="
         install/hwe-netboot/ubuntu-installer/amd64/boot-screens/txt.cfg
         install/netboot/ubuntu-installer/amd64/boot-screens/rqtxt.cfg"
 
+PRESEED_FILES="
+        lanner.cfg
+        lanner-base.seed
+        lanner-nca-4010.seed
+"
+
 function error_exit () {
     echo "$1" >&2
     exit "${2:-1}"
 }
 
+# Replace with a test for ubuntu USB.
 echo "Verifying boot menu files."
 
 for i in ${BOOT_FILES}; do
     [ -f "${BASE_PATH}/${i}" ] || error_exit "Could not find boot menu file '${BASE_PATH}/${i}'."
 done
 
-echo "Converting boot menu files."
+echo "Installing files."
 
-for i in ${BOOT_FILES}; do
-    FILENAME="${BASE_PATH}/${i}"
+echo "... ${BASE_PATH}/boot/grub/grub.cfg"
+cp "${SCRIPT_DIR}/files/grub.cfg" "${BASE_PATH}/boot/grub/grub.cfg"
 
-    echo "... ${FILENAME}"
-
-    mv "${FILENAME}" "${FILENAME}.orig"
-    ${SCRIPT_DIR}/prepare_cfg.sh "${FILENAME}.orig" "${FILENAME}"
+for i in ${PRESEED_FILES}; do
+    echo "... ${BASE_PATH}/preseed/${i}"
+    cp "${SCRIPT_DIR}/files/${i}" "${BASE_PATH}/preseed/${i}"
 done
+
+#echo "Converting boot menu files."
+
+# for i in ${BOOT_FILES}; do
+#     FILENAME="${BASE_PATH}/${i}"
+
+#     echo "... ${FILENAME}"
+
+#     mv "${FILENAME}" "${FILENAME}.orig"
+#     ${SCRIPT_DIR}/prepare_cfg.sh "${FILENAME}.orig" "${FILENAME}"
+# done
 
 echo "Done."
--- ubuntu/prepare_cfg.sh
+++ ubuntu/prepare_cfg.sh
@@ -3,4 +3,4 @@
 SRC_FILENAME="${1:?Source filename not provided.}"
 DST_FILENAME="${2:?Destination filename not provided.}"
 
-cat "${SRC_FILENAME}" | sed -e '/^append / s/ quiet//' -e '/^append / s/ splash//' -e '/^append / s/$/ console=tty0 console=ttyS0,115200n8/' > "${DST_FILENAME}"
+cat "${SRC_FILENAME}" | sed -e '/^[[:space:]]*append / s/ quiet//' -e '/^[[:space:]]*append / s/ splash//' -e '/^[[:space:]]*append / s/$/ console=tty0 console=ttyS0,115200n8/' > "${DST_FILENAME}"
